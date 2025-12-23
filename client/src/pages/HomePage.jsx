039de9abfa6351da86042617b08da3044f9f7bc1
Jeff Pilon <jeff.pilon@farmersinsurance.com>
2025-12-23 15:27:24 -0500

    homepage company logo

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,4 +1,5 @@
 import { Container } from 'react-bootstrap';
+import logoImage from '../assets/images/star_movers_logo_1.jpg';
 
 /**
  * HomePage Component
@@ -9,6 +10,21 @@ function HomePage() {
     <Container className="py-5">
       <h1>Welcome to Star Movers</h1>
       <p className="lead">Your trusted partner for all your moving needs.</p>
+      
+      {/* Star Movers Logo */}
+      <div className="d-flex justify-content-center my-4">
+        <img
+          src={logoImage}
+          alt="Star Movers Logo"
+          style={{
+            width: '75%',
+            height: 'auto',
+            borderRadius: '15px',
+            border: '2px solid #dee2e6'
+          }}
+        />
+      </div>
+      
       <p>This is the home page. Content will be added later.</p>
     </Container>
   );
